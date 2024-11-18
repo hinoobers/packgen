0f4f2da57e3fc12fa905b99178c904f7bf95016e
hinoob <redisolenmina1@gmail.com>
2024-11-18 23:53:24 +0200

    Initial

---
+++ src/client/java/org/hinoob/packgen/PackgenClient.java
@@ -0,0 +1,10 @@
+package org.hinoob.packgen;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PackgenClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
